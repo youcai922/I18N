3663754a3f305546d1c482afff807e0c331613b7
youcai922 <2287360627@qq.com>
2022-10-13 15:16:28 +0800

    controller层加上对msg的处理方法

---
+++ src/main/java/com/cet/eem/config/MyResponseBodyAdvice.java
@@ -0,0 +1,42 @@
+package com.cet.eem.config;
+
+//import com.cet.eem.common.model.Result;
+//import com.cet.eem.util.MessageUtil;
+//import com.cet.electric.authservice.common.entity.ApiResult;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @author yucan
+ * @date 2022/10/13 14:54
+ */
+@ControllerAdvice
+public class MyResponseBodyAdvice implements ResponseBodyAdvice {
+    /**
+     * 判断是否执行beforeBodyWrite方法的Controller，返回true表示执行，false表示不执行
+     */
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    /**
+     * 对返回值进行加工处理
+     */
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+//        if (body instanceof ApiResult && ((ApiResult<?>) body).getMsg() == null) {
+//            ((ApiResult<?>) body).setMsg(MessageUtil.getMessage(((ApiResult<?>) body).getCode().toString()));
+//            return body;
+//        } else if (body instanceof Result && ((Result<?>) body).getMsg() != null) {
+//            ((Result<?>) body).setMsg(MessageUtil.getMessage(((Result<?>) body).getMsg()));
+//        } else {
+//            return body;
+//        }
+        return body;
+    }
+}
